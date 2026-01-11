353a2b0de0be4eb9329719f3a59d3b2842cfcb27
Thoma <98554519+thomaaf69@users.noreply.github.com>
2026-01-11 11:56:02 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/nodelens/main.go
@@ -0,0 +1,23 @@
+// cmd/nodelens/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "nodelens/internal/nodelens"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := nodelens.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
